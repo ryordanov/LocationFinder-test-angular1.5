ad569adcbc2ed7f25ca06ee75e7b449eb019f16f
Rosen Yordanov <rosen.yordanov@softwaregroup.com>
2018-06-05 10:10:12 +0300

    fix: heroku procfile

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 Building a simple web-app with Angular 1.5, Bootstrap, Fontawesome Bootstrapcheatsheets...
 
-Running: "npm run start-local"
+Running: "npm start"
---
+++ web.js
@@ -0,0 +1,11 @@
+var port = Number(process.env.PORT || 8000);
+console.log('process.env', process.env);
+
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname + '/app'));
+
+var server = app.listen(port, function() {
+    console.log('Listening on port %d', server.address().port);
+});
